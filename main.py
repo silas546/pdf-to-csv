184d5c4f6f4baf377ce9396b40077df7f6f0c5c2
Silas-Owings <silas.owings@7summitsinc.com>
2023-08-24 14:49:59 -0500

    Added lines for writing notes

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fpdf import FPDF
 import pandas as pd
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
+pdf.set_auto_page_break(auto=False, margin=0)
 
 df = pd.read_csv("topics.csv")
 
@@ -12,13 +13,31 @@ for index, row in df.iterrows():
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"],
          align="L", ln=1)
+    for y in range(20, 298, 10):
+        pdf.line(10, y, 200, y)
 
     # per line 4, coordinates calculated in millimeters
-    pdf.line(10, 21, 200, 21)
+    pdf.ln(265)
+
+    # Set footer on master page
+    pdf.set_font(family="Times", style="I", size=8)
+    pdf.set_text_color(180, 180, 180)
+    pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
 
     # calculate the number of additional pages to add
     additional_pages = row["Pages"] - 1
     for i in range(additional_pages):
         pdf.add_page()
+
+        # Set the footer on child pages
+
+        # Height of master cell + default .ln height
+        pdf.ln(277)
+        pdf.set_font(family="Times", style="I", size=8)
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+
+        for y in range(20, 298, 10):
+            pdf.line(10, y, 200, y)
 
 pdf.output("output.pdf")
